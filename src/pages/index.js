e565a05a9d0955c10f5a537b6154777ac47efd8e
Sam Sheppard <sam.sheppard@puregym.com>
2019-10-05 20:46:52 +0100

    rework ready for displaying real content

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,30 +3,29 @@ import { Container, Row, Col } from "reactstrap";
 import Link from "gatsby-link";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
-import Scrollable from "../components/scroll/scrollable"
+import Scrollable from "../components/scroll/scrollable";
 
-const handleScroll = (event) => {
-    //console.log("Index's handleScroll");
-  }
+const handleScroll = event => {
+  //console.log("Index's handleScroll");
+};
 
 const IndexPage = ({ data }) => {
-  const { markdownRemark: post } = data;
+  const { markdownRemark } = data;
+  const { frontmatter } = markdownRemark;
   return (
     <Layout>
       <header className="masthead">
-      <Scrollable onWindowScroll={handleScroll}></Scrollable>
+        <Scrollable onWindowScroll={handleScroll}></Scrollable>
         <Container className="container h-100">
-          <Row className="h-100 align-items-center justify-content-center text-center">
-            <Col lg="10" className="align-self-end">
-              <h1 className="text-uppercase text-white font-weight-bold">
-                {data.site.siteMetadata.title} | {post.frontmatter.title}
-              </h1>
-              <hr className="divider my-4" />
-            </Col>
-            <Col lg="8" className="align-self-baseline">
-              <p className="text-white-75 font-weight-light mb-5">
-                {post.frontmatter.strapline}
-              </p>
+          <Row>
+            <Col md="12">
+              <img
+                src="https://lorempixel.com/200/400/"
+                alt="introduction image"
+                className="float-right"
+              />
+              <p className="text-white-75">{frontmatter.introduction}</p>
+              {frontmatter.body}
             </Col>
           </Row>
         </Container>
@@ -44,7 +43,8 @@ export const pageQuery = graphql`
       frontmatter {
         path
         title
-        strapline
+        introduction,
+        body
       }
     }
     site {
--- src/templates/home.js
+++ src/templates/home.js
@@ -2,32 +2,37 @@ import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
-import Scrollable from "../components/scroll/scrollable"
+import Scrollable from "../components/scroll/scrollable";
 
-const handleScroll = (event) => {
+const handleScroll = event => {
   var mainNav = document.getElementById("mainNav");
-  var pageContentRect = document.getElementById("page-content").getBoundingClientRect();
-  if(pageContentRect.top < -120) {
+  var pageContentRect = document
+    .getElementById("page-content")
+    .getBoundingClientRect();
+  if (pageContentRect.top < -120) {
     mainNav.classList.add("navbar-scrolled");
   } else {
     mainNav.classList.remove("navbar-scrolled");
   }
-}
+};
 
 export default function Template({ data }) {
-  const { markdownRemark: post } = data;
+  const { markdownRemark } = data;
+  const { frontmatter } = markdownRemark;
   return (
     <Layout>
       <header className="masthead">
-      <Scrollable onWindowScroll={handleScroll}></Scrollable>
+        <Scrollable onWindowScroll={handleScroll}></Scrollable>
         <Container className="container h-100">
           <Row>
-          <Col lg='8' className="align-self-baseline">
-              <p className="text-white-75 font-weight-light mb-5">
-              {post.frontmatter.strapline}
-              </p>
-            </Col>
-            <Col md='4' className="align-self-end">
+            <Col md="8">
+              <img
+                src="https://lorempixel.com/200/400/"
+                alt="introduction image"
+                className="float-right"
+              />
+              <p className="text-white-75">{frontmatter.introduction}</p>
+              {frontmatter.body}
             </Col>
           </Row>
         </Container>
@@ -38,12 +43,13 @@ export default function Template({ data }) {
 
 export const homePageQuery = graphql`
   query HomePage {
-    markdownRemark(frontmatter: { path: { eq:"/" } }) {
+    markdownRemark(frontmatter: { path: { eq: "/" } }) {
       html
       frontmatter {
         path
         title
-        strapline
+        introduction
+        body
       }
     }
     site {
